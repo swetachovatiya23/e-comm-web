aad667b95b4a06a9092da39b5fc5d7461a45bcfa
swetachovatiya23 <swetachovatiya2312@gmail.com>
2022-05-16 11:51:30 -0400

    git navbar block added

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,10 +1,16 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div`
+  height: 60px;
+  background-color: teal;
+`;
 
 const Navbar = () => {
     return (
-        <div>
+        <Container>
             Navbar
-        </div>
+        </Container>
     )
 }
 
