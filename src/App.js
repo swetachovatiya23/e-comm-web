532abbb6bfe6e2154d83f366d49aaff39b910b2e
swetachovatiya23 <swetachovatiya2312@gmail.com>
2022-05-19 16:18:51 -0400

    Shopping Cart Added

--- src/App.js
+++ src/App.js
@@ -4,14 +4,17 @@ import Home from "./pages/Home";
 import ProductList from "./pages/ProductList";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
+import Cart from "./pages/Cart";
 
 const App = () => {
   return (
     // <Home/>
     // <ProductList/>
     // <Product/>
-    // <Register/>
-    <Login/>
+    //<Register/>
+    // <Login/>
+    <Cart/>
+    
     
   )
 }
