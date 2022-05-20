ab1dc0b4cb595f67e1b0f6ef43be333d03bf0068
swetachovatiya23 <swetachovatiya2312@gmail.com>
2022-05-20 11:04:51 -0400

    DBConnection Succfull

--- BE/index.js
+++ BE/index.js
@@ -1,5 +1,15 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+
+// require('dotenv').config({ path: require('find-config')('.env') });
+
+
+mongoose.connect("mongodb+srv://swetac:shell@cluster0.nvuu4.mongodb.net/?retryWrites=true&w=majority"
+).then(() => console.log("DBConnection Successfull"))
+.catch((err) => {
+    console.log(err);
+});
 
 app.listen(500, ()=>{
     console.log("Backend server is running!")
