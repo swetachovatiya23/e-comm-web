ff0dc190bd35eafea7a83f13670c9115f7508cde
swetachovatiya23 <swetachovatiya2312@gmail.com>
2022-05-20 15:21:45 -0400

    Router endpoint checkin done

--- BE/index.js
+++ BE/index.js
@@ -7,13 +7,17 @@ const dotenv = require("dotenv");
 
 dotenv.config();
 
-
-mongoose.connect(
-    process.env.MONGO_URL
-).then(() => console.log("DBConnection Successfull"))
+mongoose
+.connect(process.env.MONGO_URL)
+.then(() => console.log("DBConnection Successfull"))
 .catch((err) => {
     console.log(err);
 });
+
+app.get("/api/test", () => {
+    console.log("test is successfull");
+});
+
 
 app.listen(process.env.PORT || 5000, ()=>{
     console.log("Backend server is running!")
