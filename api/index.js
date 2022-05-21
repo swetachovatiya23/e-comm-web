1450cc96309b1269d8a87105c246d59e498282ae
swetachovatiya23 <swetachovatiya2312@gmail.com>
2022-05-20 21:13:46 -0400

    auth user detail added

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const userRoute = require("./routes/user");
+const authRoute = require("./routes/auth");
 
 // require('dotenv').config({ path: require('find-config')('.env') });
 
@@ -16,7 +17,7 @@ mongoose
 });
 
 app.use(express.json());
-
+app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 
 app.listen(process.env.PORT || 5000, ()=>{
